eb960dea8d5011e3688ce18ac1a5df5151be425d
kimjihoon3106 <kimjihoon3106@gmail.com>
2024-05-12 06:56:52 +0900

    build : modal

--- app.js
+++ app.js
@@ -19,3 +19,14 @@ function checkIt(){
         }
     
 }
+const modalOpenButton = document.getElementById('modalOpenButton');
+const modalCloseButton = document.getElementById('modalCloseButton');
+const modal = document.getElementById('modalContainer');
+
+modalOpenButton.addEventListener('click', () => {
+  modal.classList.remove('hidden');
+});
+
+modalCloseButton.addEventListener('click', () => {
+  modal.classList.add('hidden');
+});
